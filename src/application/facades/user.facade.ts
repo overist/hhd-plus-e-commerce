b3aca3b3cb412b732caba9509c422494c1942f19
overist <wj@over.ist>
2025-11-21 09:42:54 +0900

    feat: 낙관락 제어를 위한 트랜잭션 제거

--- src/application/facades/user.facade.ts
+++ src/application/facades/user.facade.ts
@@ -16,15 +16,13 @@ export class UserFacade {
 
   /**
    * ANCHOR user.chargeBalance
-   * 트랜잭션으로 동시성 제어
+   * 낙관적 잠금으로 동시성 제어
    */
   async chargeBalance(
     userId: number,
     amount: number,
   ): Promise<UserBalanceView> {
-    const user = await this.prisma.runInTransaction(async () => {
-      return await this.userService.chargeUser(userId, amount);
-    });
+    const user = await this.userService.chargeUser(userId, amount);
 
     return {
       userId: user.id,
