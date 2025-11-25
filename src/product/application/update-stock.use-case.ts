b4f45e5736858965e082401af69728de64e9c1e8
overist <wj@over.ist>
2025-11-25 19:10:52 +0900

    fix: 트랜잭션 누락

--- src/product/application/update-stock.use-case.ts
+++ src/product/application/update-stock.use-case.ts
@@ -1,20 +1,27 @@
 import { Injectable } from '@nestjs/common';
 import { ProductDomainService } from '@/product/domain/services/product.service';
+import { PrismaService } from '@common/prisma-manager/prisma.service';
 import { UpdateStockCommand, UpdateStockResult } from './dto/update-stock.dto';
 
 @Injectable()
 export class UpdateStockUseCase {
-  constructor(private readonly productService: ProductDomainService) {}
+  constructor(
+    private readonly productService: ProductDomainService,
+    private readonly prisma: PrismaService,
+  ) {}
 
   /**
    * ANCHOR 관리자 상품 재고 수정
+   * 트랜잭션으로 조회-수정 원자성 보장
    */
   async execute(cmd: UpdateStockCommand): Promise<UpdateStockResult> {
-    await this.productService.updateProductOptionStock(
-      cmd.productOptionId,
-      cmd.operation,
-      cmd.quantity,
-    );
+    await this.prisma.runInTransaction(async () => {
+      await this.productService.updateProductOptionStock(
+        cmd.productOptionId,
+        cmd.operation,
+        cmd.quantity,
+      );
+    });
 
     return {};
   }
